8e5c7c1b8298a2b671a54c3362321f0b2f066e6a
Puniabor <keexavier@hotmail.com>
2022-02-13 04:55:00 +0800

    Initial

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { HiMenuAlt4 } from 'react-icon/hi';
+import { AiOutlineClose } from 'react-icon/ai';
+import logo from '../../images/kok.png'
+
+const Navbar = () => {
+    return (
+        <nav className="w-full flex md:justify-center justify-between item-center p-4">
+
+        </nav>
+    );
+}
+
+export default Navbar;
